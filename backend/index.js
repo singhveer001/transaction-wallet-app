dd7d4719cce35bdf677ed9316ae4f9ca89d716b5
singhveer001 <veerbhansingh70@gmail.com>
2024-04-18 20:52:36 +0530

    added 404 message to all invalid route reqs

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,24 @@ const express = require("express");
 const mainRouter = require("./routes/index")
 const cors = require("cors");
 require('dotenv').config()
+
+const app= express();
 app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
-const app= express();
-const mainRouter = require("./routes/index")
+
+app.get("/", (req, res) => {
+    console.log("Hello");
+    return res.status(200).send("Hello from the server!");
+})
 
 app.use("/api/v1",mainRouter)
  
+app.get('*', function (req, res) {
+    res.status(404).json({ message: "Route Not Found!" });
+});
+
 app.listen(PORT ,()=>{
     console.log(`Server running on ${PORT}`)
 })
